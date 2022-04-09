bc60a626c5eaa836b1f301fe291a5a880b01c78b
Carl Hamilton <carl@mousetrap.com>
2022-04-08 18:29:30 -0700

    Added mousetrap_toolchains module version 0.1.4.

--- get_module_sri.sh
+++ get_module_sri.sh
@@ -1,1 +1,3 @@
-echo sha256-$(curl --silent --location https://github.com/carlwhamilton/${1}/archive/refs/tags/v${2}.tar.gz | openssl dgst -sha256 -binary | openssl base64 -A)
+#!/bin/sh
+URL="https://github.com/carlwhamilton/${1}/archive/refs/tags/v${2}.tar.gz"
+echo "sha256-$(curl --silent --location ${URL} | openssl dgst -sha256 -binary | openssl base64 -A)"
