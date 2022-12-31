895f1b16226a8470719b568e830a87f95da23402
Carl Hamilton <carl@mousetrap.com>
2022-12-30 18:22:16 -0800

    Release mousetrap_toolchains v0.1.5.

--- get_module_sri.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-URL="https://github.com/carlwhamilton/${1}/archive/refs/tags/v${2}.tar.gz"
-echo "sha256-$(curl --silent --location ${URL} | openssl dgst -sha256 -binary | openssl base64 -A)"
